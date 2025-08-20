886efe1866f0c34276439493cc2e5e9891a6cb3e
Shreya Kalra <Shreyaa.kalra@gmail.com>
2025-08-20 21:11:41 +0530

    inorder traversal of binary tree

---
+++ Binary Trees/inorderTraversal.cpp
@@ -0,0 +1,33 @@
+// inorder traversal : left right root
+// TC : O(n)
+// SC : O(n)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node{
+  int data;
+  struct Node* left;
+  struct Node* right;
+  
+  Node(int val){
+    data = val;
+    left = right = nullptr;
+  }
+};
+
+void inorder(Node* node){
+  if(node==nullptr) return;
+  inorder(node->left);
+  cout << node->data << " ";
+  inorder(node->right);
+}
+
+int main() 
+{
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root ->right = new Node(3);
+    root ->left->right = new Node(6);
+    inorder(root); // 2 6 1 3
+}
