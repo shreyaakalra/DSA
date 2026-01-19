17955727e3046ef8dd6496a0631482211dbbcca6
Shreya Kalra <Shreyaa.kalra@gmail.com>
2026-01-20 01:15:59 +0530

    rename

--- Binary Trees/Iterative-Traversals/postorderUsing1Stack.cpp
+++ Binary Trees/Iterative-Traversals/postorderUsing1Stack.cpp
@@ -11,34 +11,3 @@ struct TreeNode{
     left = right = nullptr;
   }
 };
-
-class Solution {
-public:
-    vector<int> postorderTraversal(TreeNode* root) {
-
-        if(!root) return{};
-
-        stack<TreeNode*> s1;
-        stack<TreeNode*> s2;
-        vector<int> ans;
-
-        s1.push(root);
-
-        while(!s1.empty()){
-            TreeNode* curr = s1.top();
-            s1.pop();
-
-            s2.push(curr);
-
-            if(curr->left) s1.push(curr->left);
-            if(curr->right) s1.push(curr->right);
-        }
-
-        while(!s2.empty()){
-            ans.push_back(s2.top()->val);
-            s2.pop();
-        }
-
-        return ans;
-    }
-};
