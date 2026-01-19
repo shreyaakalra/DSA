930b1c5c326c81287fb39963d9039efca7963de9
Shreya Kalra <Shreyaa.kalra@gmail.com>
2026-01-20 00:53:15 +0530

    inorder traversal by iterative method

--- Binary Trees/Iterative-Traversals/inorder.cpp
+++ Binary Trees/Iterative-Traversals/inorder.cpp
@@ -12,3 +12,17 @@ struct TreeNode{
   }
 };
 
+class Solution {
+public:
+    void solve(TreeNode* root, vector<int> &ans){
+        if(root==nullptr) return;
+        solve(root->left, ans);
+        ans.push_back(root->val);
+        solve(root->right, ans);
+    }
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        solve(root, ans);
+        return ans;
+    }
+};
